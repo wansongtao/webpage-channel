d59b56dac2da3861dcb0949405f17e645799f949
wansongtao <18101837209@163.com>
2026-04-21 20:57:45 +0800

    feat: export LocalStorageAdapter in index file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { default as WebpageChannel } from './core/webpage-channel';
 export { default as BroadcastChannelAdapter } from './core/broadcast-channel-adapter';
+export { default as LocalStorageAdapter } from './core/localstorage-adapter';
 export { default as PostMessageAdapter } from './core/postmessage-adapter';
 export { default as EventBus } from './core/event-bus';
 export * from './types/index.d';

2bd7944ac838ba9b4bbab9179c66736c67c35188
wansongtao <18101837209@163.com>
2026-04-21 20:55:41 +0800

    feat: add utility function to check support for localStorage

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,7 @@
 export const isSupportBroadcastChannel = (): boolean => {
   return typeof BroadcastChannel !== 'undefined';
 };
+
+export const isSupportLocalStorage = (): boolean => {
+  return typeof localStorage !== 'undefined';
+};
